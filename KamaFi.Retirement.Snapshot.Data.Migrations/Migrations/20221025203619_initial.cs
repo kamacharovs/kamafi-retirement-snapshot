91dd1984a8232db62a841cd5221f43f03aba1f69
Georgi Kamacharov <gkamacharov@gmail.com>
2022-10-25 16:45:10 -0400

    add data

---
+++ KamaFi.Retirement.Snapshot.Data.Migrations/Migrations/20221025203619_initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace KamaFi.Retirement.Snapshot.Data.Migrations.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "retirement_fact",
+                columns: table => new
+                {
+                    retirement_fact_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    short_description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    long_description = table.Column<string>(type: "character varying(5000)", maxLength: 5000, nullable: false),
+                    year = table.Column<int>(type: "integer", nullable: false),
+                    key = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    value = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "current_timestamp"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "current_timestamp")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_retirement_fact", x => x.retirement_fact_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "retirement_fact");
+        }
+    }
+}
